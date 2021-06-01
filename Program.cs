d04c5ff39caf381f81c6d497d857e6c9a2a32171
SwicaseJia <SwicaseJia@gamil.com>
2021-06-01 11:06:58 +0800

    第一测试提交

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        /// <summary>
+        /// 主函数注释
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("名字");
+            Console.WriteLine((new Class1()).GetName());
+
+            System.Console.WriteLine("年龄");
+            Console.WriteLine(GetAge());
+            Console.ReadLine();
+            Class1 cla = new Class1();
+            cla.GetName();
+        }
+        static int GetAge()
+        {
+            return 1;
+
+        }
+    }
+}
